6e5f4ac67ad470df9956b0f137ddc87f5bb4e2fa
osjoberg <oskar@oskarsjoberg.se>
2015-05-27 23:43:22 +0200

    Fixed a bug since MSBuild no longer guarantees that the Task will finish on the same thread as it started.

--- Logger/TaskIdentifier.cs
+++ Logger/TaskIdentifier.cs
@@ -3,13 +3,11 @@
     struct TaskIdentifier
     {
         public int NodeId { get; private set; }
-        public int ThreadId { get; private set; }
         public int TaskId { get; private set; }
 
-        public TaskIdentifier(int nodeId, int threadId, int taskId) : this()
+        public TaskIdentifier(int nodeId, int taskId) : this()
         {
             NodeId = nodeId;
-            ThreadId = threadId;
             TaskId = taskId;
         }
     }
